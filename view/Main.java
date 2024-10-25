3bc360551018a49634ffdd312e3d522e83919cb9
Guilherme Roesler <guilhermeroesler.ss020@academico.ifsul.edu.br>
2024-10-24 21:46:21 -0300

    Update Main.java

--- view/Main.java
+++ view/Main.java
@@ -12,20 +12,5 @@ public class Main {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-//		Scanner sc = new Scanner(System.in);
-//		
-//		System.out.println("Para quem você gostaria de mandar o e-mail?");
-//		String to = sc.next();
-//		
-//		System.out.println("Qual o assunto do e-mail?");
-//		String subject = sc.next();
-//		
-//		System.out.println("Digite aqui a mensagem:");
-//		String messageBody = sc.nextLine();
-//		
-//		EmailSender.sendEmail(to, subject, messageBody);
-//
-//		sc.close();
 	}
 }
